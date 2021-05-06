022dd56ee366fa07656390eb2e64e37d5f92ba2c
Sergey Rogalskiy <rogalskiy.sergey@gmail.com>
2021-05-06 10:49:53 +0300

    supper hot fix of Favorited Articles (to RUS)

--- src/components/ProfileFavorites.js
+++ src/components/ProfileFavorites.js
@@ -34,7 +34,7 @@ class ProfileFavorites extends Profile {
           <Link
             className="nav-link"
             to={`/@${this.props.profile.username}`}>
-            My Articles
+            Мои записи
           </Link>
         </li>
 
@@ -42,7 +42,7 @@ class ProfileFavorites extends Profile {
           <Link
             className="nav-link active"
             to={`/@${this.props.profile.username}/favorites`}>
-            Favorited Articles
+            Любимые записи
           </Link>
         </li>
       </ul>
