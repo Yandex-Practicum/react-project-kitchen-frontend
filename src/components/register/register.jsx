3178d091938f4eef05780a08d4bdb1cbfc9ced18
Ilya Volkov <102101100@mail.ru>
2021-05-07 23:08:39 +0300

    del: comment code

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import PropTypes from 'prop-types';
-// import { Link } from "react-router-dom";
 import ListErrors from "../ListErrors";
 import agent from "../../agent";
 import { connect } from "react-redux";
