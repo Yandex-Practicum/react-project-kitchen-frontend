8c17224398441340990d4b569be00f9cb770811b
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-05-07 12:01:23 +0300

    fixed title for ArticlePreview

--- src/components/Home/MainView.js
+++ src/components/Home/MainView.js
@@ -1,4 +1,4 @@
-import ArticleList from '../ArticleList';
+import ArticleList from '../ArticleList/ArticleList';
 import React from 'react';
 import agent from '../../agent';
 import { connect } from 'react-redux';
