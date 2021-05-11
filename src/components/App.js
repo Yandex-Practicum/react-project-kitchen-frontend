107066fa75a3c59f804c218c8f11d4385e1d201d
eglazkov <egorglazkov@mail.ru>
2021-05-11 20:50:41 +0300

    Оформил компонент Article в отдельном файле article.jsx и вынес его экспорт в index.js для сокращения вложенности импорта.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { APP_LOAD, REDIRECT } from '../constants/actionTypes';
 import { Route, Switch } from 'react-router-dom';
-import Article from '../components/Article';
+import Article from '../components/article';
 import Editor from '../components/editor';
 import Home from '../components/Home';
 import Login from '../components/login';
