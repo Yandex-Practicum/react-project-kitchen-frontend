63af3557af6a075e1ec8e8b09a7ec66a7860e4d3
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-07-11 15:52:34 +0300

    FEAT: overflow in articles container in profile

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,38 +1,28 @@
+import React from 'react';
 import ArticlePreview from '../ArticlePreview/ArticlePreview';
 import ListPagination from '../ListPagination/ListPagination';
-import React from 'react';
-import styles from './ArticleList.module.scss'
+import cn from 'classnames';
+import styles from './ArticleList.module.scss';
 
-const ArticleList = props => {
+const ArticleList = (props) => {
   if (!props.articles) {
-    return (
-      <div className={[styles.article__container, styles.empty__content].join(' ')}>Данные загружаются..</div>
-    );
+    return <div className={[styles.article__container, styles.empty__content].join(' ')}>Данные загружаются..</div>;
   }
 
   if (props.articles.length === 0) {
-    return (
-      <div className={[styles.article__container, styles.empty__content].join(' ')}>
-        Здесь пусто... пока что.
-      </div>
-    );
+    return <div className={[styles.article__container, styles.empty__content].join(' ')}>Здесь пусто... пока что.</div>;
   }
 
   return (
-    <div className = {styles.article__container}>
-    <ul className = {styles.list}>
-      {
-        props.articles.map(article => {
+    <div className={styles.article__container}>
+      <ul className={cn(styles.list, { [styles.listProfile]: props.profile })}>
+        {props.articles.map((article) => {
           return (
-            <ArticlePreview article={article} key={article.slug} token={props.token} currentUser={props.currentUser}/>
+            <ArticlePreview article={article} key={article.slug} token={props.token} currentUser={props.currentUser} />
           );
-        })
-      }
-    </ul>
-      <ListPagination
-        pager={props.pager}
-        articlesCount={props.articlesCount}
-        currentPage={props.currentPage} />
+        })}
+      </ul>
+      <ListPagination pager={props.pager} articlesCount={props.articlesCount} currentPage={props.currentPage} />
     </div>
   );
 };
