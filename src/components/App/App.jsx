9a5f180407d87868e73e09555da5d9bd863b74ab
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-07-13 11:46:21 +0300

    FEAT:App to FC

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import agent from '../../agent';
 import Header from '../Header/Header';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { APP_LOAD, CHANGE_THEME, REDIRECT } from '../../constants/actionTypes';
 import { Route, Switch } from 'react-router-dom';
@@ -11,7 +11,6 @@ import Login from '../Auth/Login/Login';
 import Profile from '../Profile/Profile';
 import Register from '../Auth/Register/Register';
 import Settings from '../Settings/Settings';
-import { store } from '../../store';
 import Preloader from '../Preloader/Preloader';
 
 const mapStateToProps = (state) => {
@@ -30,124 +29,114 @@ const mapDispatchToProps = (dispatch) => ({
   onChangeTheme: () => dispatch({ type: CHANGE_THEME }),
 });
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.root = document.querySelector(':root');
-    this.darkTheme = {
-      back: '#1F2023',
-      dark: '#5299ff',
-      darkText: '#ffffff',
-      grayText: '#ffffff',
-      bannerColor: '#27292D',
-      primary: '#FFB852',
-      tagsBlock: '#27292D',
-      tags: '#27292D',
-      tagsBorder: '#0B0C0D',
-      buttonShadow: 'rgba(255, 144, 14, 0.35)',
-      redHeart: '#F53D5C',
-      deleteBtn: '',
-      commentContainer: '#27292D',
-      commentInput: '#2D2F34',
-      previewLink: '#5299ff',
-    };
-    this.whiteTheme = {
-      back: '#ffffff',
-      dark: '#212121',
-      darkText: '#0A0A0B',
-      grayText: '#62626A',
-      bannerColor: '#f4f4f6',
-      primary: '#0000FF',
-      tagsBlock: '#f4f4f6',
-      tags: 'rgb(241, 241, 241)',
-      tagsBorder: '#CCCCCC',
-      buttonShadow: 'rgba(0, 0, 255, 0.25)',
-      redHeart: '#F53D5C',
-      deleteBtn: '',
-      commentContainer: '#ffffff',
-      commentInput: '#f4f4f6',
-      previewLink: '#0000FF',
-    };
-  }
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.redirectTo) {
-      // this.context.router.replace(nextProps.redirectTo);
-      // store.dispatch(push(nextProps.redirectTo));
-      // this.props.onRedirect();
-    }
-  }
-  UNSAFE_componentWillMount() {
+const App = (props) => {
+  const root = document.querySelector(':root');
+  const darkTheme = {
+    back: '#1F2023',
+    dark: '#5299ff',
+    darkText: '#ffffff',
+    grayText: '#ffffff',
+    bannerColor: '#27292D',
+    primary: '#FFB852',
+    tagsBlock: '#27292D',
+    tags: '#27292D',
+    tagsBorder: '#0B0C0D',
+    buttonShadow: 'rgba(255, 144, 14, 0.35)',
+    redHeart: '#F53D5C',
+    deleteBtn: '',
+    commentContainer: '#27292D',
+    commentInput: '#2D2F34',
+    previewLink: '#5299ff',
+  };
+  const whiteTheme = {
+    back: '#ffffff',
+    dark: '#212121',
+    darkText: '#0A0A0B',
+    grayText: '#62626A',
+    bannerColor: '#f4f4f6',
+    primary: '#0000FF',
+    tagsBlock: '#f4f4f6',
+    tags: 'rgb(241, 241, 241)',
+    tagsBorder: '#CCCCCC',
+    buttonShadow: 'rgba(0, 0, 255, 0.25)',
+    redHeart: '#F53D5C',
+    deleteBtn: '',
+    commentContainer: '#ffffff',
+    commentInput: '#f4f4f6',
+    previewLink: '#0000FF',
+  };
+  useEffect(() => {
     const token = window.localStorage.getItem('jwt');
     if (token) {
       agent.setToken(token);
     }
-    this.props.onLoad(token ? agent.Auth.current() : null, token);
-  }
-  componentDidMount() {
+    props.onLoad(token ? agent.Auth.current() : null, token);
     if (localStorage.getItem('isDarkTheme') === 'true') {
-      this.props.onChangeTheme();
+      props.onChangeTheme();
     }
-    this.root.style = `--white:${this.whiteTheme.back};
-      --dark:${this.whiteTheme.dark};
-      --dark-text:${this.whiteTheme.darkText};
-      --gray-text:${this.whiteTheme.grayText};
-      --banner-color:${this.whiteTheme.bannerColor};
-      --primary:${this.whiteTheme.primary};
-      --tags-block:${this.whiteTheme.tagsBlock};
-      --tags:${this.whiteTheme.tags};
-      --tags-border:${this.whiteTheme.tagsBorder};
-      --button-shadow:${this.whiteTheme.buttonShadow};
-      --red-heart:${this.whiteTheme.redHeart};
-      --delete-btn:${this.whiteTheme.deleteBtn};
-      --comment-container:${this.whiteTheme.commentContainer};
-      --comment-input:${this.whiteTheme.commentInput};
-      --preview-link:${this.whiteTheme.previewLink};
-      `;
-  }
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
-    localStorage.setItem('isDarkTheme', nextProps.isDarkTheme);
-    nextProps.isDarkTheme
-      ? (this.root.style = `--white:${this.darkTheme.back};
-      --dark:${this.darkTheme.dark};
-      --dark-text:${this.darkTheme.darkText};
-      --gray-text:${this.darkTheme.grayText};
-      --banner-color:${this.darkTheme.bannerColor};
-      --tags-block:${this.darkTheme.tagsBlock};
-      --primary:${this.darkTheme.primary};
-      --tags:${this.darkTheme.tags};
-      --tags-border:${this.darkTheme.tagsBorder};
-      --button-shadow:${this.darkTheme.buttonShadow};
-      --red-heart:${this.darkTheme.redHeart};
-      --delete-btn:${this.darkTheme.deleteBtn};
-      --comment-container:${this.darkTheme.commentContainer};
-      --comment-input:${this.darkTheme.commentInput};
-      --preview-link:${this.darkTheme.previewLink};
-      --preview-link-hover:${this.darkTheme.primary};
-      `)
-      : (this.root.style = `--white:${this.whiteTheme.back};
-      --dark:${this.whiteTheme.dark};
-      --dark-text:${this.whiteTheme.darkText};
-      --gray-text:${this.whiteTheme.grayText};
-      --banner-color:${this.whiteTheme.bannerColor};
-      --primary:${this.whiteTheme.primary};
-      --tags-block:${this.whiteTheme.tagsBlock};
-      --tags:${this.whiteTheme.tags};
-      --tags-border:${this.whiteTheme.tagsBorder};
-      --button-shadow:${this.whiteTheme.buttonShadow};
-      --red-heart:${this.whiteTheme.redHeart};
-      --delete-btn:${this.whiteTheme.deleteBtn};
-      --comment-container:${this.whiteTheme.commentContainer};
-      --comment-input:${this.whiteTheme.commentInput};
-      --preview-link:${this.whiteTheme.previewLink};
-      --preview-link-hover:${this.whiteTheme.darkText};
-      `);
-    return true;
-  }
-  render() {
-    if (this.props.appLoaded) {
+    root.style = `--white:${whiteTheme.back};
+                  --dark:${whiteTheme.dark};
+                  --dark-text:${whiteTheme.darkText};
+                  --gray-text:${whiteTheme.grayText};
+                  --banner-color:${whiteTheme.bannerColor};
+                  --primary:${whiteTheme.primary};
+                  --tags-block:${whiteTheme.tagsBlock};
+                  --tags:${whiteTheme.tags};
+                  --tags-border:${whiteTheme.tagsBorder};
+                  --button-shadow:${whiteTheme.buttonShadow};
+                  --red-heart:${whiteTheme.redHeart};
+                  --delete-btn:${whiteTheme.deleteBtn};
+                  --comment-container:${whiteTheme.commentContainer};
+                  --comment-input:${whiteTheme.commentInput};
+                  --preview-link:${whiteTheme.previewLink};
+                  `;
+    //eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('isDarkTheme', props.isDarkTheme);
+    props.isDarkTheme
+      ? (root.style = `--white:${darkTheme.back};
+            --dark:${darkTheme.dark};
+            --dark-text:${darkTheme.darkText};
+            --gray-text:${darkTheme.grayText};
+            --banner-color:${darkTheme.bannerColor};
+            --tags-block:${darkTheme.tagsBlock};
+            --primary:${darkTheme.primary};
+            --tags:${darkTheme.tags};
+            --tags-border:${darkTheme.tagsBorder};
+            --button-shadow:${darkTheme.buttonShadow};
+            --red-heart:${darkTheme.redHeart};
+            --delete-btn:${darkTheme.deleteBtn};
+            --comment-container:${darkTheme.commentContainer};
+            --comment-input:${darkTheme.commentInput};
+            --preview-link:${darkTheme.previewLink};
+            --preview-link-hover:${darkTheme.primary};
+            `)
+      : (root.style = `--white:${whiteTheme.back};
+            --dark:${whiteTheme.dark};
+            --dark-text:${whiteTheme.darkText};
+            --gray-text:${whiteTheme.grayText};
+            --banner-color:${whiteTheme.bannerColor};
+            --primary:${whiteTheme.primary};
+            --tags-block:${whiteTheme.tagsBlock};
+            --tags:${whiteTheme.tags};
+            --tags-border:${whiteTheme.tagsBorder};
+            --button-shadow:${whiteTheme.buttonShadow};
+            --red-heart:${whiteTheme.redHeart};
+            --delete-btn:${whiteTheme.deleteBtn};
+            --comment-container:${whiteTheme.commentContainer};
+            --comment-input:${whiteTheme.commentInput};
+            --preview-link:${whiteTheme.previewLink};
+            --preview-link-hover:${whiteTheme.darkText};
+            `);
+    //eslint-disable-next-line
+  }, [props.isDarkTheme]);
+
+  const render = () => {
+    if (props.appLoaded) {
       return (
         <>
-          <Header appName={this.props.appName} currentUser={this.props.currentUser} />
+          <Header appName={props.appName} currentUser={props.currentUser} />
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
@@ -164,14 +153,11 @@ class App extends React.Component {
     }
     return (
       <div>
-        <Header appName={this.props.appName} currentUser={this.props.currentUser} />
+        <Header appName={props.appName} currentUser={props.currentUser} />
       </div>
     );
-  }
-}
+  };
 
-// App.contextTypes = {
-//   router: PropTypes.object.isRequired
-// };
-
+  return render();
+};
 export default connect(mapStateToProps, mapDispatchToProps)(App);
