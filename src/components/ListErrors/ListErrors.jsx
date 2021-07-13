e840467e6a0248fdb67b5507a5d0f26145bedd9f
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-07-13 19:28:45 +0300

    FEAT: component ListErrors to FC

--- src/components/ListErrors/ListErrors.jsx
+++ src/components/ListErrors/ListErrors.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 
-class ListErrors extends React.Component {
-  render() {
-    const errors = this.props.errors;
+const ListErrors = (props) => {
+  const errors = props.errors;
+  const render = () => {
     if (errors) {
       return (
         <ul className="error-messages">
-          {
-            Object.keys(errors).map(key => {
-              return (
-                <li key={key}>
-                  {key} {errors[key]}
-                </li>
-              );
-            })
-          }
+          {Object.keys(errors).map((key) => {
+            return (
+              <li key={key}>
+                {key} {errors[key]}
+              </li>
+            );
+          })}
         </ul>
       );
     } else {
       return null;
     }
-  }
-}
+  };
+  return render();
+};
 
 export default ListErrors;
