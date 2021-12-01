5827c015806e14c988e782723737289b7f65fc88
Irina Abah <irin.bon@gmail.com>
2021-12-01 00:36:00 +0000

    style: adjusted styles of navigation links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header({appName, isLoggedIn, currentUser}) {
   return (
     <header className={headerStyles.header}>
       <div className={headerStyles.container}>
-        <Link to="/" className={headerStyles.name}>
+        <Link exact to="/" className={headerStyles.name}>
           {appName}
         </Link>
         <Navigation isLoggedIn={isLoggedIn} currentUser={currentUser}/>
--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -2,20 +2,31 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import navigationStyles from './Navigation.module.css';
 import homePath from '../../../images/home-icon.svg';
+import loginPath from '../../../images/login-icon.svg';
+import articlePath from '../../../images/newArticle-icon.svg';
+import settingsPath from '../../../images/settings-icon.svg';
+import profilePath from '../../../images/profile-icon.svg'
 
 function Navigation({isLoggedIn, currentUser}) {
   return (
     <nav className={navigationStyles.nav}>
-      {!isLoggedIn ? (
+      {isLoggedIn ? (
         <ul className={navigationStyles.list}>
-          <li>
-            <img src={homePath} alt="Home icon" />
-            <NavLink to="/" className={navigationStyles.link} activeClassName={navigationStyles.active}>
+          <li className={navigationStyles.list_item}>
+            <NavLink 
+            exact to="/" 
+            className={navigationStyles.link} 
+            activeClassName={navigationStyles.active}>
+            <img className={navigationStyles.icon} src={homePath} alt="Home icon" />
               Главная
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/login" className={navigationStyles.link} activeClassName={navigationStyles.active}>
+          <li className={navigationStyles.list_item}> 
+            <NavLink 
+            to="/login" 
+            className={navigationStyles.link} 
+            activeClassName={navigationStyles.active}>
+            <img className={navigationStyles.icon} src={loginPath} alt="Login icon" />
               Войти
             </NavLink>
           </li>
@@ -23,25 +34,39 @@ function Navigation({isLoggedIn, currentUser}) {
       ): (
         <ul className={navigationStyles.list}>
           <li className={navigationStyles.list_item}>
-            <NavLink to="/" className="nav-link">
+            <NavLink 
+            exact to="/" 
+            className={navigationStyles.link} 
+            activeClassName={navigationStyles.active}>
+            <img className={navigationStyles.icon} src={homePath} alt="Home icon" />
               Главная
             </NavLink>
           </li>
           <li className={navigationStyles.list_item}>
-            <NavLink to="/editor" className="nav-link">
-              <i className="ion-compose"></i>&nbsp;Новая запись
+            <NavLink 
+            to="/editor" 
+            className={navigationStyles.link} 
+            activeClassName={navigationStyles.active}>
+            <img className={navigationStyles.icon} src={articlePath} alt="Article icon" />
+              Новая запись
             </NavLink>
           </li>
           <li className={navigationStyles.list_item}>
-            <NavLink to="/settings" className="nav-link">
-              <i className="ion-gear-a"></i>&nbsp;Настройки
+            <NavLink 
+            to="/settings" 
+            className={navigationStyles.link} 
+            activeClassName={navigationStyles.active}>
+            <img className={navigationStyles.icon} src={settingsPath} alt="Settings icon" />
+              Настройки
             </NavLink>
           </li>
-          <li className={navigationStyles.list_item}>
+          <li className={navigationStyles.list_item}> 
             <NavLink
               to='/account'
-              className="nav-link">
-              <span>Hello, User</span>
+              className={navigationStyles.link} 
+              activeClassName={navigationStyles.active}>
+              <img className={navigationStyles.icon} src={profilePath} alt="Profile icon" />
+              <span>User User</span>
             </NavLink>
           </li>
         </ul>
