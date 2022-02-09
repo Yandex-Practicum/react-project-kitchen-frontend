f08dd01e584d693536cb275075a81d8353ccb375
Alexander Ilnykh <AlexanderIlnykhDev@yandex.ru>
2022-02-09 11:32:06 +0500

    moved ArticlePreview to ArticleList

--- src/components/common/ArticleList/ArticleList.jsx
+++ src/components/common/ArticleList/ArticleList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ListPagination from '../../ListPagination';
-import ArticlePreview from '../ArticlePreview/ArticlePreview';
+import ArticlePreview from './ArticlePreview/ArticlePreview';
 
 const ArticleList = (props) => {
   if (!props.articles) {
