778b98e4952c40aab686477af2be8ec1e9e1824c
Roman <ismagilov771@gmail.com>
2022-04-21 00:34:32 +0300

    added ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import React from 'react';
 import { store, history} from './store';
 
 import { Route, Switch } from 'react-router-dom';
@@ -8,8 +8,10 @@ import { ConnectedRouter } from 'react-router-redux';
 
 import App from './components/App';
 
+const storeProp: any = { store };
+
 ReactDOM.render((
-  <Provider store={store}>
+  <Provider {...storeProp}>
     <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={App} />
