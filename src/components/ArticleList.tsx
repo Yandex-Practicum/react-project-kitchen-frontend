eba0f38edc0f716a9812c15073db8e33349f6fe3
Artem <zhiglov.artem@yandex.ru>
2022-04-22 16:28:46 +0500

    refactor: ArticleList

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -2,7 +2,11 @@ import ArticlePreview from './ArticlePreview';
 import ListPagination from './ListPagination';
 import React from 'react';
 
-const ArticleList = props => {
+interface TArticleListProps {
+  articles: any;
+}
+
+const ArticleList: React.FC<TArticleListProps> = (props: any) => {
   if (!props.articles) {
     return (
       <div className="article-preview">Loading...</div>
@@ -20,7 +24,7 @@ const ArticleList = props => {
   return (
     <div>
       {
-        props.articles.map(article => {
+        props.articles.map((article: any) => {
           return (
             <ArticlePreview article={article} key={article.slug} />
           );
