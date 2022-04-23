63e8c9879000fc939578a9cb0c344ff20feb3297
George051191 <trubacheff_91@mail.ru>
2022-04-23 16:05:18 +0300

    fixes

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, legacy_createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
@@ -21,5 +21,5 @@ const getMiddleware = () => {
     }
 };
 
-export const store = legacy_createStore(
+export const store = createStore(
     reducer, composeWithDevTools(getMiddleware()));
