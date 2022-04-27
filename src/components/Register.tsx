e137c5101ebc0d7dc2a2311966cd807e5f17ebb9
Yan Samoilov <janchik31@yandex.ru>
2022-04-27 11:11:01 +0300

    Refactor Profile and children components, Register to slice

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,10 +2,6 @@ import { Link } from 'react-router-dom';
 import ListErrors from './ListErrors';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   REGISTER,
-//   REGISTER_PAGE_UNLOADED
-// } from '../constants/actionTypes';
 import {
   REGISTER, REGISTER_PAGE_UNLOADED
 } from '../services/authSlice';
