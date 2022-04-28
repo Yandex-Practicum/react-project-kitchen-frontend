b1b39b557a0beceb32780ed570469b9066281009
George051191 <trubacheff_91@mail.ru>
2022-04-28 23:19:21 +0300

    fixes done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from '../services/hooks';
 import { Profile } from '../components_refact';
+import Header from './Header';
 
 import {
   Route, Routes, useNavigate, useLocation,
 } from 'react-router-dom';
-/*
-import Header from './Header';
-import Article from './Article';
-import { Editor } from '../components_refact';
-import Home from './Home';
-import Login from './Login';
-import Profile from './Profile';
-import ProfileFavorites from './ProfileFavorites';
-import Register from './Register';
-import Settings from './Settings';
-*/
+
 import { loadInitialDataThunk } from '../thunks';
 
 const App = () => {
@@ -29,86 +20,24 @@ const App = () => {
   }, [dispatch]);
 
   return (
-
-
     <div>
-
+      <Header />
       <Routes>
-        {/* <Route exact path='/' element={<Home />} />
-     <Route path='/login' element={<Login />} />
-     <Route path='/register' element={<Register />} />
-     <Route path='/editor/:slug' element={<Editor />} />
-     <Route path='/editor' element={<Editor />} />
-     <Route path='/article/:id' element={<Article />} />
-     <Route path='/settings' element={<Settings />} />
-     <Route path='/@:username/favorites' element={<ProfileFavorites />} /> */}
-        <Route exact path='/@:username' element={<Profile />} />
+        <Route exact path='/' element={<div>загрузка...</div>} />
+        {/*   <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/editor/:slug' element={<Editor />} />
+        <Route path='/editor' element={<Editor />} />
+        <Route path='/article/:id' element={<Article />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/@:username/favorites' element={<ProfileFavorites />} />  */}
+        <Route path='/@:username' element={<Profile />} />
       </Routes>
+    </div>
+  );
+
+};
 
 
-
-    </div >
-    /*  
-     
- 
-); 
-
-return (<Profile />);
-}
-
-/*
-)
-class App extends React.Component {
-
-componentWillReceiveProps(nextProps) {
- if (nextProps.redirectTo) {
-   // this.context.router.replace(nextProps.redirectTo);
-   store.dispatch(this.props.navigate(nextProps.redirectTo));
-   this.props.onRedirect();
- }
-}
-componentWillMount() {
- const token = window.localStorage.getItem('jwt');
- if (token) {
-   agent.setToken(token);
- }
- this.props.onLoad(token ? agent.Auth.current() : null, token);
-}
-
-render() {
- if (this.props.appLoaded) {
-   return (
-     <div>
-       <Header
-         appName={this.props.appName}
-         currentUser={this.props.currentUser} />
-         <Routes>
-         <Route exact path="/" component={Home}/>
-         <Route path="/login" component={Login} />
-         <Route path="/register" component={Register} />
-         <Route path="/editor/:slug" component={Editor} />
-         <Route path="/editor" component={Editor} />
-         <Route path="/article/:id" component={Article} />
-         <Route path="/settings" component={Settings} />
-         <Route path="/@:username/favorites" component={ProfileFavorites} />
-         <Route path="/@:username" component={Profile} />
-         </Routes>
-     </div>
-   );
- }
- return (
-   <div>
-     <Header
-       appName={this.props.appName}
-       currentUser={this.props.currentUser} />
-   </div>
- );
-}
-}
-
-// App.contextTypes = {
-//   router: PropTypes.object.isRequired
-// }; */
-  )
-}
 export default App;
