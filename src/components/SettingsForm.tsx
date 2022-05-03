93a8fe8fa7e9d1c96dcc0f9fe07180d5658d5bec
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-05-03 17:00:48 +0300

    fix conflict logic of setting_page

--- src/components/SettingsForm.tsx
+++ src/components/SettingsForm.tsx
@@ -1,122 +1,152 @@
-import React, { useEffect, useState, useMemo } from "react";
+import ListErrors from "./ListErrors";
+import  { useEffect, useState, FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUserThunk } from "../services/thunks";
+import { logout as logoutAction } from "../services/commonSlice";
+import { useHistory } from "react-router";
 
-interface TSettingsFormProps {
-  currentUser: any;
-  onSubmitForm: any;
+interface ISettingsForm {
+  setIsUpdatedInfoMsg: (isUpdatedInfoMsg: boolean) => void
 }
 
-const SettingsForm: React.FC<TSettingsFormProps> = (props: any) => {
-
-  const [ state, setState ] = useState ({
-    image: '',
-    username: '',
-    bio: '',
-    email: '',
-    password: ''
+const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
+  const { currentUser } = useSelector((state: any) => state.common);
+  const dispatch = useDispatch();
+  const [formValues, setFormvalues] = useState({
+    image: "",
+    username: "",
+    bio: "",
+    email: "",
+    password: "",
   });
-  const [ isProgress, setIsProgress ] = useState(false);
-
-  const updateState = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState({...state, [field]: e.target.value})
-  };
-
-  const submitForm = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (!state.password) {
-      setState({...state, password: ''})
-    };
-
-    setIsProgress(true);
-
-    props.onSubmitForm(state);
-  };
-
 
   useEffect(() => {
-    if (props.currentUser) {
-      Object.assign(state, {
-        image: props.currentUser.image || '',
-        username: props.currentUser.username,
-        bio: props.currentUser.bio,
-        email: props.currentUser.email
-      });
+    if (currentUser) {
+      setFormvalues({ ...currentUser, password: "" });
     }
-  }, [])
+  }, [currentUser]);
 
- useMemo(()=> {
-  if (props.currentUser) {
-    setState(Object.assign({}, state, {
-      image: props.currentUser.image || '',
-      username: props.currentUser.username,
-      bio: props.currentUser.bio,
-      email: props.currentUser.email
-    }));
-  }
- },[props])
+  const updateForm = (e: { target: { name: string; value: string }}) => {
+    setFormvalues({ ...formValues, [e.target.name]: e.target.value });
+  };
 
-    return (
-      <form onSubmit={submitForm}>
-        <fieldset>
+  const onSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    dispatch(updateUserThunk(formValues))
+      .unwrap()
+      .then(() => {
+        setIsUpdatedInfoMsg(true);
+        setTimeout(() => {
+          setIsUpdatedInfoMsg(false);
+        }, 1500);
+      });
+  };
 
-          <fieldset className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="URL of profile picture"
-              value={state.image}
-              onChange={updateState('image')} />
-          </fieldset>
-
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="text"
-              placeholder="Username"
-              value={state.username}
-              onChange={updateState('username')} />
-          </fieldset>
-
-          <fieldset className="form-group">
-            <textarea
-              className="form-control form-control-lg"
-              rows="8"
-              placeholder="Short bio about you"
-              value={state.bio}
-              onChange={updateState('bio')}>
-            </textarea>
-          </fieldset>
-
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="email"
-              placeholder="Email"
-              value={state.email}
-              onChange={updateState('email')} />
-          </fieldset>
-
-          <fieldset className="form-group">
-            <input
-              className="form-control form-control-lg"
-              type="password"
-              placeholder="New Password"
-              value={state.password}
-              onChange={updateState('password')} />
-          </fieldset>
-
-          <button
-            className="btn btn-lg btn-primary pull-xs-right"
-            type="submit"
-            disabled={isProgress}
-            //нужно написать переключение dissabled кнопки
-            >
-            Update Settings
-          </button>
-
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset>
+        <fieldset className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="URL of profile picture"
+            value={formValues.image}
+            name="image"
+            onChange={updateForm}
+          />
         </fieldset>
-      </form>
-    );
-}
 
-export { SettingsForm };
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="text"
+            placeholder="username"
+            value={formValues.username}
+            name="username"
+            onChange={updateForm}
+          />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <textarea
+            className="form-control form-control-lg"
+            rows={8}
+            placeholder="Short bio about you"
+            value={formValues.bio}
+            name="bio"
+            onChange={updateForm}
+          ></textarea>
+        </fieldset>
+
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="email"
+            placeholder="Email"
+            value={formValues.email}
+            name="email"
+            onChange={updateForm}
+          />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <input
+            className="form-control form-control-lg"
+            type="password"
+            placeholder="New Password"
+            value={formValues.password}
+            name="password"
+            onChange={updateForm}
+          />
+        </fieldset>
+
+        <button
+          className="btn btn-lg btn-primary pull-xs-right"
+          type="submit"
+          disabled={false}
+        >
+          Update Settings
+        </button>
+      </fieldset>
+    </form>
+  );
+  // }
+};
+
+const Settings = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [isUpdatedInfoMsg, setIsUpdatedInfoMsg] = useState(false);
+
+  const logout = () => {
+    dispatch(logoutAction());
+    history.push("/");
+  };
+
+  return (
+    <div className="settings-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Your Settings</h1>
+
+            {isUpdatedInfoMsg && <h4 className="text-xs-center">Updated</h4>}
+
+            {/* <ListErrors errors={this.props.errors}></ListErrors> */}
+
+            <SettingsForm setIsUpdatedInfoMsg={setIsUpdatedInfoMsg} />
+
+            <hr />
+
+            <button className="btn btn-outline-danger" onClick={logout}>
+              Or click here to logout.
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  // }
+};
+
+export default Settings;
