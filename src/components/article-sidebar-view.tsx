1ea41908e884328e4f193a1daf44ba52274b2d3f
AStoyakin <Alexander.Stoyakin@advdigital.ru>
2022-05-05 18:39:00 +0300

    feature: создать компонент для боковой панели с информацией

---
+++ src/components/article-sidebar-view.tsx
@@ -0,0 +1,16 @@
+import React, {FunctionComponent} from "react";
+
+const ArticleSidebarView: FunctionComponent = () => {
+  return (
+    <>
+      <img src="" alt="фото профиля"/>
+      <div>
+        <h3>Имя Фамилия</h3>
+        <p>01.01.1999</p>
+      </div>
+
+    </>
+  )
+}
+
+export default ArticleSidebarView;
---
+++ src/components/sidebar-information.tsx
@@ -0,0 +1,10 @@
+import React, {FunctionComponent} from "react";
+
+const SidebarInformation: FunctionComponent = () => {
+  return (
+    <h2>Заголовок</h2>
+
+  )
+}
+
+export default SidebarInformation;
