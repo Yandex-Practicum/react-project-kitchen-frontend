b4bf912c806e7369ac4efda175e96f13bc44d4b3
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-05-08 11:44:19 +0300

    tags styled

--- src/components/Home/Tags.tsx
+++ src/components/Home/Tags.tsx
@@ -1,43 +1,57 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { getArticlesByTag } from '../../api';
-import { getAllArticlesByTagThunk } from '../../services/thunks';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getArticlesByTag } from "../../api";
+import { getAllArticlesByTagThunk } from "../../services/thunks";
+import {
+  CancelButtonDiv,
+  Tag,
+  TagsContainer,
+} from "../StyledComponents/tagsStyles";
 
 type TTagsProps = {
-  tags: string[] | null,
-  onClickTag: (tag: string, pager: (page: any) => {}, payload: any) => {}
-}
+  tags: string[] | null;
+  onClickTag: (tag: string, pager: (page: any) => {}, payload: any) => {};
+};
 
-const Tags: React.FC<TTagsProps> = props => {
-  const dispatch = useDispatch()
+type TCancelButtonProps = {
+  tag: string;
+};
+
+const CancelButton: React.FC<TCancelButtonProps> = (tag, setIsActive) => {
+  return <CancelButtonDiv />;
+};
+
+const Tags: React.FC<TTagsProps> = (props) => {
+  const dispatch = useDispatch();
   const tags = props.tags;
+
   if (tags) {
     return (
-      <div className="tag-list">
-        {
-          tags.map(tag => {
-            const handleClick = (ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-              ev.preventDefault();
-              // props.onClickTag(tag, page => agent.Articles.byTag(tag, page), agent.Articles.byTag(tag));
-              dispatch(getAllArticlesByTagThunk({ tag: tag, page: 0 }));
-            };
-            return (
-              <a
-                href=""
-                className="tag-default tag-pill"
-                key={tag}
-                onClick={handleClick}>
-                {tag}
-              </a>
-            );
-          })
-        }
-      </div>
+      <TagsContainer>
+        {tags.map((tag) => {
+          const [isActive, setIsActive] = useState(false);
+
+          const handleClick = (
+            ev: React.MouseEvent<HTMLButtonElement, MouseEvent>
+          ) => {
+            ev.preventDefault();
+            dispatch(getAllArticlesByTagThunk({ tag: tag, page: 0 }))
+              .unwrap()
+              .then(() => {
+                isActive ? setIsActive(false) : setIsActive(true);
+              });
+          };
+          return (
+            <Tag key={tag} isActive={isActive} onClick={handleClick}>
+              {tag}
+              {isActive && <CancelButton tag={tag} />}
+            </Tag>
+          );
+        })}
+      </TagsContainer>
     );
   } else {
-    return (
-      <div>Loading Tags...</div>
-    );
+    return <div>Loading Tags...</div>;
   }
 };
 
---
+++ src/components/StyledComponents/tagsStyles.ts
@@ -0,0 +1,48 @@
+import { colors, device } from "./constantsStyles";
+import styled from "styled-components";
+import blueCross from "../../images/blueCross.svg";
+
+export const TagsContainer = styled.div`
+  max-width: 100%;
+  display: flex;
+  gap: 24px;
+`;
+
+type TTagProps = {
+  isActive: boolean;
+  onClick: (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+};
+
+export const Tag = styled.button<TTagProps>`
+  margin: 0;
+  padding: 0;
+  outline: none;
+  border: none;
+  display: flex;
+  gap: 4px;
+  font-family: "AlegreyaSans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${(props) => (props.isActive ? colors.blue : colors.darkGrey)};
+  &:hover {
+    color: ${colors.blue};
+    text-decoration: none;
+  }
+  &:focus {
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+  }
+`;
+
+export const CancelButtonDiv = styled.button`
+  margin: 0;
+  padding: 0;
+  border: none;
+  width: 24px;
+  height: 24px;
+  background: url(${blueCross});
+`;
