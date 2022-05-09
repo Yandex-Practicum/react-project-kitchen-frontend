8d36a85ed708d3da2e426e16dfbfca10b7a48e7b
Yan Samoilov <janchik31@yandex.ru>
2022-05-09 19:13:30 +0300

    Add validation and other fixes

--- src/components/settings-form.tsx
+++ src/components/settings-form.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState, FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUserThunk } from "../services/thunks";
-import { logout as logoutAction } from "../services/commonSlice";
-import { useHistory } from "react-router";
 import { useForm } from "react-hook-form";
-import * as Styles from "../components/StyledComponents/settingsStyles";
 import SignupLoginSubmitBtn from "../components/SignupLoginSubmitBtn";
 import IconInput from "../UI/icon-input/icon-input";
 import IconInputFile from "../UI/icon-input-file/icon-input-file";
+import * as Styles from "../components/StyledComponents/settingsStyles";
 
 interface ISettingsForm {
   setIsUpdatedInfoMsg: (isUpdatedInfoMsg: boolean) => void;
@@ -21,13 +19,14 @@ type FormData = {
 };
 
 const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
-  const {currentUser}  = useSelector((state: any) => state.common)
+  const { currentUser } = useSelector((state: any) => state.common)
   const dispatch = useDispatch();
   const [isError, setIsError] = useState(false);
   const [errorsResponse, setErrorsResponse] = useState<any>(null);
   const [visible, setVisible] = useState(false);
+  const { inProgress } = useSelector((state: any) => state.settings);
 
-  const {register, formState: { errors, isValid }, handleSubmit,
+  const { register, setValue, getValues, formState: { errors, isValid }, handleSubmit,
   } = useForm<FormData>({
     mode: "onChange",
     defaultValues: {
@@ -42,45 +41,21 @@ const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
     e && e.preventDefault();
     dispatch(updateUserThunk({ image, username, email, password }))
       .unwrap()
-      .then(() => {
-        setIsUpdatedInfoMsg(true);
-        setTimeout(() => {
-          setIsUpdatedInfoMsg(false);
-        }, 1500);
-      })
       .catch((error: any) => {
         setErrorsResponse({ [error.name]: error.message });
       });
-    });
+  });
 
-      useEffect(() => {
-        setIsError(isValid)
-      },)
-       
-  // useEffect(() => {
-  //   if (currentUser) {
-  //     setFormvalues({ ...currentUser, image: "", password: "" });
-  //   }
-  // }, [currentUser]);
+  useEffect(() => {
+    setIsError(isValid)
+  })
 
-  // const updateForm = (e: { target: { name: string; value: string } }) => {
-  //   setFormvalues({ ...formValues, [e.target.name]: e.target.value });
-  // };
+  useEffect(() => {
+    setValue('username', currentUser.username);
+    setValue('email', currentUser.email);
+  },[currentUser.username, currentUser.email])
 
-  // const onSubmit = (e: { preventDefault: () => void }) => {
-  //   e.preventDefault();
-  //   dispatch(updateUserThunk(formValues))
-  //     .unwrap()
-  //     .then(() => {
-  //       setIsUpdatedInfoMsg(true);
-  //       setTimeout(() => {
-  //         setIsUpdatedInfoMsg(false);
-  //       }, 1500);
-  //     })
-  //     .catch((error: any) => {
-  //       setErrorsResponse({ [error.name]: error.message });
-  //     });
-  // };
+  const isDisabled = getValues('password') === "" || inProgress;
 
   const onToggle = () => {
     setVisible((visible) => !visible);
@@ -89,39 +64,27 @@ const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
   return (
     <Styles.SettingsForm action="POST" onSubmit={handleSubmitForm}>
       <Styles.SettingsFieldSet>
+
         <Styles.SettingsLabel>
           URL изображения профиля
           <Styles.SettingsInputContainer>
             <Styles.SettingsInput
-              // isError={false}
-              // type="url"
-              // placeholder="url"
-              // value={formValues.image}
-              // name="image"
-              // onChange={updateForm}
-            
-              isError={errors.username}
+              isError={errors.image}
               {...register("image", {
-                // required: "Это поле обязательно к заполнению.",
                 pattern: {
-                  value:  /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/,
-                  message: "Введите url.",
+                  value: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+                  message: "Введите корректный url.",
                 },
-                // minLength: {
-                //   value: 2,
-                //   message: "Имя должно быть не менее двух букв.",
-                // },
               })}
-          />
+            />
             <Styles.SettingsIcon>
               <IconInputFile />
             </Styles.SettingsIcon>
           </Styles.SettingsInputContainer>
-          {/* <Styles.ErrorsContainer>
-            {errors?.image && <Styles.SettingsError>{errors?.image?.message}</Styles.SettingsError>}
-        </Styles.ErrorsContainer>  */}
         </Styles.SettingsLabel>
-        {/* second */}
+        <Styles.ErrorsContainer>
+          {errors?.image && <Styles.SettingsError>{errors?.image?.message}</Styles.SettingsError>}
+        </Styles.ErrorsContainer>
 
         <Styles.SettingsLabel>
           Имя пользователя
@@ -137,53 +100,33 @@ const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
                 message: "Имя должно быть не менее двух букв.",
               },
             })}
-            // isError={false}
-            // type="text"
-            // placeholder="Имя пользователя"
-            // value={formValues.username}
-            // name="username"
-            // onChange={updateForm}
-            // isError={errors.username}
-            // {...register("username", {
-            //   pattern: {
-            //     value: /[a-zA-Z0-9]$/,
-            //     message: "Имя пользователя может содержать буквы кириллицы и цифры.",
-            //   },
-            //   minLength: {
-            //     value: 2,
-            //     message: "Имя должно быть не менее двух букв.",
-            //   },
-            // })}
           />
         </Styles.SettingsLabel>
-        {/* third*/}
+        <Styles.ErrorsContainer>
+          {errors?.username && <Styles.AutoError>{errors?.username?.message}</Styles.AutoError>}
+        </Styles.ErrorsContainer>
+
         <Styles.SettingsLabel>
           E-mail
           <Styles.SettingsInput
-            // isError={false}
-            // type="email"
-            // placeholder="Email"
-            // value={formValues.email}
-            // name="email"
-            // onChange={updateForm}
             isError={errors.email}
-            placeholder={"Пример email: name@example.com"}
             {...register("email", {
               pattern: {
-                
                 value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
                 message: "Пример email: name@example.com",
               },
             })}
           />
         </Styles.SettingsLabel>
-
-        {/* third*/}
+        <Styles.ErrorsContainer>
+          {errors?.email && <Styles.AutoError>{errors?.email?.message}</Styles.AutoError>}
+        </Styles.ErrorsContainer>
 
         <Styles.SettingsLabel>
           Новый пароль
           <Styles.SettingsInputContainer>
             <Styles.SettingsInput
+              autoComplete="new-password"
               type={visible ? "text" : "password"}
               isError={errors.password}
               {...register("password", {
@@ -196,22 +139,20 @@ const SettingsForm: FC<ISettingsForm> = ({ setIsUpdatedInfoMsg }) => {
                   message: "Пароль должен быть более 5 символов.",
                 },
               })}
-              // isError={false}
-              // type={visible ? "text" : "password"}
-              // placeholder="Новый пароль"
-              // value={formValues.password}
-              // name="password"
-              // onChange={updateForm}
             />
             <Styles.SettingsIcon>
               <IconInput visible={visible} toggle={onToggle} />
             </Styles.SettingsIcon>
           </Styles.SettingsInputContainer>
         </Styles.SettingsLabel>
+        <Styles.ErrorsContainer>
+          {errors?.password && <Styles.AutoError>{errors?.password?.message}</Styles.AutoError>}
+        </Styles.ErrorsContainer>
 
-        
+        <SignupLoginSubmitBtn btnText="Обновить настройки" disabled={!isError || isDisabled} />
+
       </Styles.SettingsFieldSet>
-      <SignupLoginSubmitBtn btnText="Обновить настройки" disabled={false} />
+
     </Styles.SettingsForm>
   );
   // }
