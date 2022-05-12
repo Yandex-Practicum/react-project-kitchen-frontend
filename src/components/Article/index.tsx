7c63235fee3663690a038be2ac741d8f2173165a
Ivan Kudriashov <pereyyra@rambler.ru>
2022-05-12 20:05:19 +0300

    Промежуточный коммит

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,14 +1,3 @@
-
-// import DOMPurify from 'dompurify';
-// import ArticleMeta from './ArticleMeta';
-// import CommentContainer from './CommentContainer';
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { marked } from 'marked';
-// import { ARTICLE_PAGE_LOADED, ARTICLE_PAGE_UNLOADED} from '../../services/articleSlice';
-// import { getArticle, getCommentsForArticle } from '../../api';
-
-
 import DOMPurify from 'dompurify';
 import { marked } from 'marked';
 import ArticleMeta from "./ArticleMeta";
@@ -17,6 +6,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getArticleThunk, getCommentsForArticleThunk } from "../../services/thunks";
 import { useParams } from "react-router";
+import Banner from '../Home/Banner';
+import { ArticlePage, ASide, PageBody, PageContent } from '../StyledComponents/articlePageStyles';
 
 type TArticleProps = {
   match: {
@@ -33,35 +24,7 @@ const Article: React.FC<TArticleProps> = (props) => {
   const { comments } = useSelector((state: any) => state.article);
   const { commentErrors } = useSelector((state: any) => state.article);
   const params: { id: string } = useParams();
-
-
-  // const onLoad = (payload: any) => {
-  //   return dispatch(ARTICLE_PAGE_LOADED(payload))
-  // }
-
-  // const onUnload = () => {
-  //   return dispatch(ARTICLE_PAGE_UNLOADED())
-  // }
-
-  // useEffect(() => {
-  //   dispatch(ARTICLE_PAGE_LOADED(Promise.all([
-  //     getArticle(props.match.params.id),
-  //     getCommentsForArticle(props.match.params.id)
-  //   ])))
-
-  //   return () => {
-  //     dispatch(ARTICLE_PAGE_UNLOADED());
-  //   };
-  // }, []);
-
-  // if (Object.keys(article).length === 0) {
-  //   return null;
-  // }
-
-  // const articleBody = DOMPurify.sanitize(article.body);
-  // const markup  = { __html: marked(articleBody)};
-
-  // const canModify = currentUser && currentUser.username === article.author.username;
+  const { appName, token } = useSelector((state: any) => state.common);
 
   useEffect(() => {
     if (params.id) {
@@ -80,47 +43,62 @@ const Article: React.FC<TArticleProps> = (props) => {
   const canModify =
     currentUser && currentUser.username === article.author.username;
 
-  return (
-    <div className="article-page">
-      <div className="banner">
-        <div className="container">
-          <h1>{article.title}</h1>
-          <ArticleMeta article={article} canModify={canModify} />
-        </div>
-      </div>
+  return (<>
+    <Banner token={token} appName={appName} />
 
-      <div className="container page">
-        <div className="row article-content">
-          <div className="col-xs-12">
-            <div dangerouslySetInnerHTML={markup}></div>
+    <ArticlePage>
 
-            <ul className="tag-list">
-              {article.tagList.map((tag: any) => {
-                return (
-                  <li className="tag-default tag-pill tag-outline" key={tag}>
-                    {tag}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
 
-        <hr />
+      <PageBody>
 
-        <div className="article-actions"></div>
+        <PageContent>
 
-        <div className="row">
-          <CommentContainer
-            comments={comments || []}
-            errors={commentErrors}
-            slug={props.match.params.id}
-            currentUser={currentUser}
-          />
-        </div>
-      </div>
-    </div>
-  );
+
+                <h1>{article.title}</h1>
+                <ArticleMeta article={article} canModify={canModify} />
+
+
+
+            <div className="container page">
+              <div className="row article-content">
+                <div className="col-xs-12">
+                  <div dangerouslySetInnerHTML={markup}></div>
+
+                  <ul className="tag-list">
+                    {article.tagList.map((tag: any) => {
+                      return (
+                        <li className="tag-default tag-pill tag-outline" key={tag}>
+                          {tag}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="article-actions"></div>
+
+              <div className="row">
+                <CommentContainer
+                  comments={comments || []}
+                  errors={commentErrors}
+                  slug={props.match.params.id}
+                  currentUser={currentUser}
+                />
+              </div>
+            </div>
+        </PageContent>
+
+        <ASide>
+
+        </ASide>
+
+      </PageBody>
+    </ArticlePage>
+
+  </>);
 };
 
 export default Article;
---
+++ src/components/StyledComponents/articlePageStyles.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+import { textColor, linkColor, inputBorderColor, device } from './constantsStyles';
+
+export const ArticlePage = styled.section`
+  max-width: 1140px;
+  margin: 0 auto;
+
+  @media ${device.laptopL} {
+    max-width: 955px;
+  }
+
+  @media ${device.laptop} {
+    max-width: 720px;
+  }
+  @media ${device.mobileS} {
+    max-width: 280px;
+  }
+`
+
+export const PageBody = styled.div`
+  display: flex;
+  gap: 81px;
+
+  margin-top: -307px;
+
+  @media ${device.laptopL} {
+    margin-top: -371px;
+  }
+
+  @media ${device.laptop} {
+    margin-top: -403px;
+    gap: 40px;
+  }
+  @media ${device.tablet} {
+    max-width: 280px;
+    margin-top: -411px;
+  }
+`
+
+export const PageContent = styled.div`
+  width: 700px;
+
+  @media ${device.laptopL} {
+    width: 588px;
+  }
+
+  @media ${device.tablet} {
+    width: 453px;
+  }
+  /* @media ${device.mobileS} {
+    max-width: (280px / 320px);
+  } */
+`
+
+export const ASide = styled.section`
+  width: 359px;
+  background-color: aqua;
+
+  @media ${device.laptopL} {
+    max-width: 377px;
+  }
+
+  @media ${device.laptop} {
+    max-width: 277px;
+  }
+  /* @media ${device.mobileS} {
+    max-width: 280px;
+  } */
+`
+export const ArticleTitle = styled.section`
+  font-size: 56px;
+  font-weight: 400;
+  line-height: 64px;
+
+  color: ${textColor.headers};
+
+  @media ${device.laptopL} {
+    max-width: 377px;
+  }
+
+  @media ${device.laptop} {
+    max-width: 277px;
+  }
+  /* @media ${device.mobileS} {
+    max-width: 280px;
+  } */
+`
