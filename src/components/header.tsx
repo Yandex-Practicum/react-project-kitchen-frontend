27182e10ff37335db7a22047ada6a6e0f07a1a64
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2022-05-12 17:40:42 +0300

    Fixed some header styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,16 @@ import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import Banner from "./Home/Banner";
 import Navigation from "./Navigation";
+import { HeaderContainer } from "./StyledComponents/headerStyles";
 
 const Header: FC = () => {
   const { appName, token } = useSelector((store: any) => store.common);
 
   return (
-    <header>
+    <HeaderContainer>
       <Navigation />
       <Banner token={token} appName={appName} />
-    </header>
+    </HeaderContainer>
   );
 };
 
