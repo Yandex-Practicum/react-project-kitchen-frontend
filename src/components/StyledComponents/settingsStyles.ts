b0eece5c4f348c78058f7c79a1977ef447e2a68a
Yan Samoilov <janchik31@yandex.ru>
2022-05-13 13:57:48 +0300

    Fix settings: inputs, button, margin

--- src/components/StyledComponents/settingsStyles.ts
+++ src/components/StyledComponents/settingsStyles.ts
@@ -3,19 +3,19 @@ import { textColor, inputBorderColor, device } from './constantsStyles';
 
 export const SettingsSection = styled.section`
   position: relative;
-  max-width: 540px;
-  margin: 0 auto;
-  padding: 56px 20px 0;
+  max-width: 580px;
+  margin: -250px auto 0;
+  padding: 0 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  @media ${device.tablet} {
-    padding-top: 48px;
+  @media ${device.laptop} {
+    margin: -358px auto 0;
   }
-  @media ${device.mobileS} {
+  /* @media ${device.mobileS} {
     padding-top: 56px;
-  }
+  } */
 `
 
 export const SettingsTitle = styled.h2`
@@ -30,7 +30,7 @@ export const SettingsTitle = styled.h2`
 
   margin-bottom: 40px;
 
-  @media ${device.tablet} {
+  @media ${device.laptop} {
     font-size: 32px;
     line-height: 36px;
 
