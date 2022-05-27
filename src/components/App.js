586adf3b3223ad252f5e1fb79459e21de72e1f3d
Vasily <wf@mail.ru>
2022-05-27 22:59:50 +0300

    refactor: ProfileFavorites has been moved to a separate folder

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import Editor from '../components/Editor';
 import Home from '../components/Home';
 import Login from '../components/Login';
 import Profile from '../components/Profile';
-import ProfileFavorites from '../components/ProfileFavorites';
+import ProfileFavorites from './ProfileFavorites/ProfileFavorites';
 import Register from '../components/Register';
 import Settings from '../components/Settings';
 import { store } from '../store';
