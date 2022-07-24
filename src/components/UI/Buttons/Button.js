8a305b3490b60f471372f7fb7214b260622c53e3
capt. Mario <ahmetzanovmarat@gmail.com>
2022-07-25 00:31:48 +0300

    add scss, update uuid, button

--- src/components/UI/Buttons/Button.js
+++ src/components/UI/Buttons/Button.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
 import styleButtons from './Buttons.module.scss';
 
-const Button = () => <button className={styleButtons.button}>Button</button>;
+const Button = ({
+  children, icon, type = 'default', disabled = false, active = false,
+}) => {
+  const types = { default: styleButtons.default, navigation: styleButtons.navigation };
+  return (
+    <button className={clsx(styleButtons.button, types[type] ? types[type] : '', active ? styleButtons.navigation_active : '')} disabled={disabled}>
+      {icon}
+      <span>{children}</span>
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  icon: PropTypes.node,
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+  active: PropTypes.bool,
+};
 
 export default Button;
