f60db820e8efb096c7f1c694d7244bbd21881894
capt. Mario <ahmetzanovmarat@gmail.com>
2022-07-28 20:14:06 +0300

    skip

--- src/components/UI/Buttons/Button.js
+++ src/components/UI/Buttons/Button.js
@@ -5,20 +5,32 @@ import clsx from 'clsx';
 import styleButtons from './Buttons.module.scss';
 
 const Button = ({
-  children, icon, type = 'default', disabled = false, active = false,
+  children,
+  icon,
+  onClick,
+  type = 'default',
+  disabled = false,
+  active = false,
+  className = '',
 }) => {
-  const types = { default: styleButtons.default, navigation: styleButtons.navigation };
+  const types = {
+    default: styleButtons.default,
+    navigation: styleButtons.navigation,
+    outline: styleButtons.outline,
+  };
 
   return (
     <button
       className={clsx(
+        className,
         styleButtons.button,
         types[type] ? types[type] : '',
         active ? styleButtons.navigation_active : '',
       )}
       disabled={disabled}
+      onClick={onClick}
     >
-      <icon.type color='secondary' hoverColor='primary'/>
+      {icon && <icon.type color='secondary' hoverColor='primary' />}
       <span>{children}</span>
     </button>
   );
@@ -27,9 +39,11 @@ const Button = ({
 Button.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.node,
+  onClick: PropTypes.func,
   type: PropTypes.string,
   disabled: PropTypes.bool,
   active: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 export default Button;
