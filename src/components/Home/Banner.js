d02b651b3e599f0fb2b78c30732d9a7101e261af
DubiumEkb <dubium-ekb@yandex.ru>
2022-08-16 22:20:31 +0500

    modified:   .gitignore 	new file:   .prettierrc.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/agent.js 	modified:   src/components/App.js 	modified:   src/components/Article/ArticleActions.js 	modified:   src/components/Article/ArticleMeta.js 	modified:   src/components/Article/Comment.js 	modified:   src/components/Article/CommentContainer.js 	modified:   src/components/Article/CommentInput.js 	modified:   src/components/Article/CommentList.js 	modified:   src/components/Article/DeleteButton.js 	modified:   src/components/Article/index.js 	modified:   src/components/ArticleList.js 	modified:   src/components/ArticlePreview.js 	modified:   src/components/Editor.js 	modified:   src/components/Header.js 	modified:   src/components/Home/Banner.js 	modified:   src/components/Home/MainView.js 	modified:   src/components/Home/Tags.js 	modified:   src/components/Home/ToDelete.js 	modified:   src/components/Home/index.js 	modified:   src/components/ListErrors.js 	modified:   src/components/ListPagination.js 	modified:   src/components/Login.js 	modified:   src/components/Profile.js 	modified:   src/components/ProfileFavorites.js 	modified:   src/components/Register.js 	modified:   src/components/Settings.js 	modified:   src/constants/actionTypes.js 	modified:   src/index.js 	modified:   src/middleware.js 	modified:   src/reducer.js 	modified:   src/reducers/article.js 	modified:   src/reducers/articleList.js 	modified:   src/reducers/auth.js 	modified:   src/reducers/common.js 	modified:   src/reducers/editor.js 	modified:   src/reducers/home.js 	modified:   src/reducers/profile.js 	modified:   src/reducers/settings.js 	modified:   src/store.js

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react"
 
 const Banner = ({ appName, token }) => {
-  if (token) {
-    return null;
-  }
-  return (
-    <div className="banner">
-      <div className="container">
-        <h1 className="logo-font">
-          {appName.toLowerCase()}
-        </h1>
-        <p>Your community project starter pack.</p>
-      </div>
-    </div>
-  );
-};
+	if (token) return null
 
-export default Banner;
+	return (
+		<div className="banner">
+			<div className="container">
+				<h1 className="logo-font">{appName.toLowerCase()}</h1>
+				<p>Your community project starter pack.</p>
+			</div>
+		</div>
+	)
+}
+
+export default Banner
