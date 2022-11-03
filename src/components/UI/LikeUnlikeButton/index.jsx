fe59216d5ca44f46c8fa25560107e14cb7daaa81
Ruslick <motsukov@gmail.com>
2022-11-03 12:47:01 +0300

    [C42-9] add like

---
+++ src/components/UI/LikeUnlikeButton/index.jsx
@@ -0,0 +1,11 @@
+import { FavoritesIcon, LikeIcon } from 'components/Icons'
+import React from 'react'
+import { Button } from '..'
+
+export const LikeUnlikeButton = ({children, onClick, favorited}) => {
+  const buttonType = favorited ? "delete" : "light"
+  const iconType = favorited ? "default" : "active"
+  return (
+    <Button onClick={onClick} type={buttonType}>{children}<FavoritesIcon type={iconType} /></Button>
+  )
+}
